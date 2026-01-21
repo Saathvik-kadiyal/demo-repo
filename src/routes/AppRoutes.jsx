fb3cf7b0ce87baab301087509cc20378b726076e
saathvik-kadiyal <saathvikkadiyal@gmail.com>
2026-01-21 15:25:43 +0530

    removed files, removed imports(comparision, clientsummary)

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,11 +2,9 @@ import { Routes, Route } from "react-router-dom";
 import { Suspense, lazy } from "react";
 import Layout from "../Layout/Layout.jsx";
 import PrivateRoute from "./PrivateRoute.jsx";
-import Comparision from "../component/Comparision.jsx";
 import ClientSummaryDetailedPage from "../pages/ClientSummaryDetailPage.jsx";
 import EmployeeEditPage from "../pages/EmployeeEditPage.jsx";
 const DashboardPage = lazy(() => import("../pages/DashboardPage.jsx"));
-const ClientSummaryPage = lazy(() => import("../pages/ClientSummaryPage.jsx"));
 const FileInput = lazy(() => import("../pages/FileInput.jsx"));
 const LoginPage = lazy(() => import("../pages/LoginPage.jsx"));
 const NotFound = lazy(() => import("../pages/NotFound.jsx"));
@@ -34,14 +32,6 @@ const AppRoutes = () => {
              </PrivateRoute>
           }
         />
-         <Route
-          path="comparison"
-          element={
-            <Suspense fallback={<div className="p-6">Loading Comparision.</div>}>
-              <Comparision />
-            </Suspense>
-          }
-        />
         <Route
           path="shift-allowance"
           element={
@@ -58,18 +48,7 @@ const AppRoutes = () => {
             </Suspense>
         }
         />
-        <Route
-          path="client-summary"
-          element={
-            <Suspense fallback={<div className="p-6">Loading Client Summary...</div>}>
-              {/* <ClientSummaryPage /> */}
-              <ClientSummaryDetailedPage/>
-            </Suspense>
-          }
-        />
       </Route>
-
-      {/* Catch-all 404 */}
       <Route
         path="*"
         element={
